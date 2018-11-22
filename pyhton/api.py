721c95e6b8558ce3c2c005f800e715c3e4d54dc0
yume_yu <yu.matchang@gmail.com>
2018-11-22 19:55:26 +0900

    とりあえずjsonを返すapiにしてみた。db接続情報は仮あて

--- pyhton/api.py
+++ pyhton/api.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template #追加
-import pymysql
+import json	#jsonを扱う?
+from flask import Flask, request, jsonify #flaskライブラリ
+import pymysql #mysql関連ライブラリ
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False #日本語文字化け対策
+app.config["JSON_SORT_KEYS"] = False #ソートをそのまま<Paste>
 
 @app.route('/')
 def hello():
@@ -12,20 +15,24 @@ def hello():
 		port=5353,
 		user='root',
 		password='example',
-		db='mysql',
+		db='manage_worker',
 		charset='utf8',
 		cursorclass=pymysql.cursors.DictCursor,
 		)
 
 	cur = db.cursor()
-	sql = "select * from user"
+	sql = "select * from employer"
 	cur.execute(sql)
 	members = cur.fetchall()
-
+	print(type(members));
+	print(members);
 	cur.close()
 	db.close()
-	#return name
-	return render_template('hello.html', title='flask test', members=members) #変更
+
+	return jsonify({
+        'status':'OK',
+        'members':members
+        })
 
 ## おまじない
 if __name__ == "__main__":
